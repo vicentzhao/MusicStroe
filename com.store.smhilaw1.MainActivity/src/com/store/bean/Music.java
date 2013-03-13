f7e93f3c8ab2109d929166707476080dc83de3fa
sl <sl@192.168.1.55>
2013-03-13 08:53:24 +0800

    音乐详情基本布局

---
+++ com.store.smhilaw1.MainActivity/src/com/store/bean/Music.java
@@ -0,0 +1,63 @@
+package com.store.bean;
+
+public class Music {
+	
+	 private String id;  //music id
+	 private String pName;  // music title
+	 private String ALBUM;  //music  
+	 private String PIC;    //imagepath
+	 private String QNAME;  //quity
+	 private String pcname;  //request
+	 private String pnname;   //way
+	 private String setname;  //set
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getpName() {
+		return pName;
+	}
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+	public String getALBUM() {
+		return ALBUM;
+	}
+	public void setALBUM(String aLBUM) {
+		ALBUM = aLBUM;
+	}
+	public String getPIC() {
+		return PIC;
+	}
+	public void setPIC(String pIC) {
+		PIC = pIC;
+	}
+	public String getQNAME() {
+		return QNAME;
+	}
+	public void setQNAME(String qNAME) {
+		QNAME = qNAME;
+	}
+	public String getPcname() {
+		return pcname;
+	}
+	public void setPcname(String pcname) {
+		this.pcname = pcname;
+	}
+	public String getPnname() {
+		return pnname;
+	}
+	public void setPnname(String pnname) {
+		this.pnname = pnname;
+	}
+	public String getSetname() {
+		return setname;
+	}
+	public void setSetname(String setname) {
+		this.setname = setname;
+	}
+	 
+   
+}
